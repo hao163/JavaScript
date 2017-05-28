527cbb6c3e24320cfd46a53feeaeb4237209c670
谭志锋 <156272132@qq.com>
2017-05-28 22:02:55 +0800

    增加文件夹

---
+++ myjs/base.js
@@ -0,0 +1,11 @@
+var Base={
+ getId:function(id){
+  return document.getElementById(id);
+ },
+ getName:function(na){
+  return document.getElementsByName(na);
+ },
+ getTag:function(tag){
+  return document.getElementsByTagName(tag);
+ },
+};
